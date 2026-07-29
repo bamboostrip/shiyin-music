e0d97d5947aba3be80e6dfbd948a15258a39d663
hank <qiheng0625@gmail.com>
2026-07-29 09:18:49 +0800

    release: v2.4.4

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -2,8 +2,8 @@ class AppConfig {
   const AppConfig._();
 
   static const appName = '时音';
-  static const appVersion = '2.4.3';
-  static const appVersionCode = '243';
+  static const appVersion = '2.4.4';
+  static const appVersionCode = '244';
 
   static const debugLyrics = bool.fromEnvironment(
     'KA_MUSIC_DEBUG_LYRICS',
